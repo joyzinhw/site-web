0f46cc95aa386b23d30aa4c8999d54dec036cc63
joyzinhw <joycemouraifpi@gmail.com>
2023-03-28 10:01:51 -0300

    web-site

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ function back()
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
+
 function calcular()
 {
     var resultado = document.getElementById('resultado').innerHTML;
